5bedb749fadadcc36340b36fa52967ae4b00b0c5
LuisDavidJv99 <luisdavidjv86@estudiantec.cr>
2019-10-05 20:57:06 -0600

    Se avanzo en la obtencion de los cuadrantes y sus colores, sin embargo falta agregar la estructura en la que se va a almacenar y mejorarlo

--- Source/GeneticProbabilisticSubgroups.py
+++ Source/GeneticProbabilisticSubgroups.py
@@ -1,5 +1,6 @@
 from PIL import Image
 from collections import Counter
+from random import randint
 
 class GenerateProbabilisticSubgroups:
 
@@ -8,30 +9,30 @@ class GenerateProbabilisticSubgroups:
     __img = ""
     __width = 0
 
-    def __init__(self):
+    def __init__(self, pCantPartitions):
         self.__Subgroups = {}
-        self.__img = Image.open('Luneos-logo-1024.png', 'r')
+        self.__img = Image.open('Luneos-logo-1024.png')
         self.__pixImage = self.__img.load()
         self.__width, self.__height = self.__img.size
-        self.__cantPartitions = 32
+        self.__cantPartitions = pCantPartitions
         self.__sizePartitions = (self.__width/self.__cantPartitions)
 
     def generateGroupsOfCuadrants(self):
-        for index in range(self.__cantPartitions):
+        for index in range(0, self.__width, int(self.__sizePartitions)):
             #for index2 in range(self.__sizePartitions * index, self.__sizePartitions * (index + 1)):
-            initialX = self.__sizePartitions * index
-            endX = self.__sizePartitions * (index + 1)
-            initialY = self.__sizePartitions * index
-            endy = self.__sizePartitions * (index + 1)
+            initialX = index
+            endX = (index + 1)
+            for index2 in range(0, self.__height, int(self.__sizePartitions)):
+                initialY = index2
+                endY = (index2 + 1)
+                for index3 in range(int(self.__width*0.6)):
+                    dato_pixel = self.__img.getpixel((randint(initialX, endX), randint(initialY, endY)))
+                    print(dato_pixel)
+
 
     def getWidth(self):
-        return self.__width
+        return self.__sizePartitions
 
 
-
-miSubgrupo = GenerateProbabilisticSubgroups()
+miSubgrupo = GenerateProbabilisticSubgroups(32)
 miSubgrupo.generateGroupsOfCuadrants()
-
-miSubgrupo.getWidth()
-
-print(2+2)
