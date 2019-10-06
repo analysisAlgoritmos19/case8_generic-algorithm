560b24ce16c15e21678c94097595b1d5ffb6a167
Esteban Salas <esalasestrada@gmail.com>
2019-10-06 12:44:15 -0600

    Cambio de nombres en variables

--- Source/GeneticProbabilisticSubgroups.py
+++ Source/GeneticProbabilisticSubgroups.py
@@ -2,9 +2,10 @@ from PIL import Image
 from collections import Counter
 from random import randint
 
+
 class GenerateProbabilisticSubgroups:
 
-    __cantPartitions: int
+    __amountPartitions: int
     __Subgroups = Counter()
     __img = ""
     __width = 0
@@ -15,20 +16,19 @@ class GenerateProbabilisticSubgroups:
         self.__pixImage = self.__img.load()
         self.__width, self.__height = self.__img.size
         self.__cantPartitions = pCantPartitions
-        self.__sizePartitions = (self.__width/self.__cantPartitions)
+        self.__sizePartitions = (self.__width / self.__cantPartitions)
 
     def generateGroupsOfCuadrants(self):
         for index in range(0, self.__width, int(self.__sizePartitions)):
-            #for index2 in range(self.__sizePartitions * index, self.__sizePartitions * (index + 1)):
+            # for index2 in range(self.__sizePartitions * index, self.__sizePartitions * (index + 1)):
             initialX = index
             endX = (index + 1)
             for index2 in range(0, self.__height, int(self.__sizePartitions)):
                 initialY = index2
                 endY = (index2 + 1)
-                for index3 in range(int(self.__width*0.6)):
+                for index3 in range(int(self.__width * 0.6)):
                     dato_pixel = self.__img.getpixel((randint(initialX, endX), randint(initialY, endY)))
                     print(dato_pixel)
-
 
     def getWidth(self):
         return self.__sizePartitions
