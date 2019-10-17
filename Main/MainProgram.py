f1738d220d9249fa6da273b6fc50d184d1e8c945
Esteban Salas <esalasestrada@gmail.com>
2019-10-17 02:45:33 -0600

    se crea el main para que muestre primero una imagen y luego la otra, solo falta que la siguiente tome los valores de poblacion en la siguiente 95%

--- Main/MainProgram.py
+++ Main/MainProgram.py
@@ -3,11 +3,16 @@ from Source.GenerateProbabilisticQuadrants import generate_probabilistic_quadran
 from Visualize.HTMLHandler import *
 
 if __name__ == '__main__':
-    sub_image_list = generate_probabilistic_quadrants("../Resources/guacamaya.jpg")
-    not_empty_sub_images = check_for_empty_quadrants(sub_image_list)
-    ready_polygons = make_genetic_parallel(not_empty_sub_images)
-    for polygons in ready_polygons:
-        for polygon in polygons:
-            take_SVG(polygon)
-    finish_SVG()
-    print(len(ready_polygons))
+    origin = "../Resources/"
+    image_names = ["guacamaya.jpg", "Beach.jpg"]
+    images_analysis = []
+    for image_name in image_names:
+        complete_name = origin+image_name
+        analysis = generate_probabilistic_quadrants(complete_name)
+        not_empty_sub_images = check_for_empty_quadrants(analysis)
+        ready_polygons = make_genetic_parallel(not_empty_sub_images)
+        for polygons in ready_polygons:
+            for polygon in polygons:
+                take_SVG(polygon)
+        finish_SVG()
+        print(len(ready_polygons))
